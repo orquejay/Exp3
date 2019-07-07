55fbf1f534595b468ebf9291a8fa7f67ee249336
orquejay <rafaeljay.curry@gmail.com>
2019-07-07 17:07:13 +0800

    three

---
+++ exp3_pr3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+	char array[50];
+	int size;
+	
+	cout << "Enter a char array.\n";
+	cin >> array;
+
+	size = 0;
+	while (array[size] != '\0')
+	size++;
+
+	for (int j = size - 1; j >= 0; j--)
+	{
+		cout << array[j];
+	}
+	
+	cout << "\nArray size is:" << size;
+
+	_getch();
+	return 0;
+}
